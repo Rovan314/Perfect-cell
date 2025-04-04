a24a99dc6f56c5005181abf44a854df1a3f322d9
Rovan314 <ridwanahtesham@gmail.com>
2025-04-04 14:48:03 +0200

    Update maine.py

--- maine.py
+++ maine.py
@@ -1,5 +1,12 @@
 print('Welcome to dodo')
-print('''1. Check if a number is even or odd
-         2. Check what is thet type of data of your input
-         3. Exit''')
-no1 = input('What would you like to do?')
+start = True
+while start:
+    no1 = int(input('Enter a number and we\'ll see if it is odd or even\n'))
+    if (no1 % 2) == 0 and int:
+        print('Even')
+    elif (no1 %2 != 0) and int:
+        print('Odd')
+    else: ValueError
+    print('Input a number')
+
+    
