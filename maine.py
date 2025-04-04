fa32642c0d75381ea2e28404d1432b7ff71666f5
Rovan314 <ridwanahtesham@gmail.com>
2025-04-04 06:40:04 +0200

    Testing something

---
+++ maine.py
@@ -0,0 +1,5 @@
+print('Welcome to dodo')
+print('''1. Check if a number is even or odd
+         2. Check what is thet type of data of your input
+         3. Exit''')
+no1 = input('What would you like to do?')
