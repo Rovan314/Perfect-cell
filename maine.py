eb8af8752e41b6ab174b5513e9571fa9c21950dd
Rovan314 <ridwanahtesham@gmail.com>
2025-04-05 12:20:21 +0200

    Update maine.py

--- maine.py
+++ maine.py
@@ -1,12 +1,20 @@
 print('Welcome to dodo')
-start = True
-while start:
-    no1 = int(input('Enter a number and we\'ll see if it is odd or even\n'))
-    if (no1 % 2) == 0 and int:
-        print('Even')
-    elif (no1 %2 != 0) and int:
-        print('Odd')
-    else: ValueError
+while True:
+    try:
+        no1 = int(input('Enter a number and we\'ll see if it is odd or even\n'))
+        if (no1 % 2) == 0 and int:
+            print('Even')
+        elif (no1 %2 != 0) and int:
+            print('Odd')
+    except: ValueError
     print('Input a number')
 
-    
+    no2 = input('Would you like to go again? \n')
+    if no2 == ('Y', 'yes', 'YES', 'Yes', 'y'):
+        continue
+    elif no2 == ('N', 'no', 'NO', 'No', 'n'):
+        print('Goodbye')
+        break
+    else:
+        print('Wrong')
+        break
