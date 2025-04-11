046a6743b4eb3b9675aa16bf99447be658186167
Rovan314 <ridwanahtesham@gmail.com>
2025-04-11 19:01:13 +0200

    Update maine.py

--- maine.py
+++ maine.py
@@ -1,55 +1,12 @@
-print('Welcome to dodo')
-def oddeven():
-    try:
-        no1 = int(input('Enter a number and we\'ll see if it is odd or even\n'))
-        if (no1 % 2) == 0 and int:
-            print('Even')
-        elif (no1 %2) != 0 and int:
-            print('Odd')
-    except ValueError:
-        print('Input a number this time')
-    
-    no2 = input('Would you like to go again? \n')
-    if no2 in ('Y', 'yes', 'YES', 'Yes', 'y'):
-        oddeven()
-    elif no2 in ('N', 'no', 'NO', 'No', 'n'):
-        Kr = input('Kamen Rider?')
-        if Kr in ('Y', 'yes', 'YES', 'Yes', 'y'):
-         def loops():
-            colors = ["Red", "Green", "Yellow", "Black", "Blue"]
-            KRider = "Rider"
-            for y in colors:
-                print(f' {y} {KRider}')
-                loops()
-        elif Kr in ('N', 'no', 'NO', 'No', 'n'):
-            print('Goodbye ?')
-            exit()
-        else:
-            print('Wrong')
+def game():
+    pass
+
+while True:
+    play = input('Welcome, wanna play? \n')
+    if play in ("Yes", 'Y', "y", "yes", "YES"):
+        game()
+    elif play in ('No', "N", "n", "no", "YES"):
+        print('Goodbye?')
+        exit()
     else:
-        print('Wrong')
-        
-oddeven()
-
-def arrae():
-    nummason = [[3, 8, 1],[5, 7, 2],[9, 4, 6]]
-    largest = nummason[0][0]
-    for w in nummason:
-        for v in w:
-            if v > largest:
-                largest = v
-                print("Largest value:", largest)
-
-arrae()
-
-def threshold():
-    nummason = [[3, 8, 1],[5, 7, 2],[9, 4, 6]]
-    threshold = 5
-    smaller = []
-    for x in nummason:
-        for y in x:
-            if y < threshold:
-                smaller.append(y)
-            print(f"Values smaller than {threshold}:", smaller)
-
-threshold()
+        print('I don\'t understand, try that again')
