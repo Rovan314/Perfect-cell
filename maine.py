7015b37c5c65b8d8e09ade721a5241bffa84c81e
Rovan314 <ridwanahtesham@gmail.com>
2025-04-12 14:45:15 +0200

    Update maine.py

--- maine.py
+++ maine.py
@@ -1,5 +1,16 @@
 def game():
-    pass
+    G = input('1 for Rock, 2 for Paper and 3 for Scissors')
+    if G in ('1'):
+        print('Currently under construction, try again later, Goodbye?')
+        exit()
+    elif G in ('2'):
+        print('Currently under construction, try again later, Goodbye?')
+        exit()
+    elif G in ('3'):
+        print('Currently under construction, try again later, Goodbye?')
+        exit()
+    else:
+        print('I don\'t understand, try that again')
 
 while True:
     play = input('Welcome, wanna play? \n')
